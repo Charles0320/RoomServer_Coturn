3bc96230fd51189efb22cb5969c43234551b7a4c
charles <charles.chen@jwaoo.com>
2018-05-29 15:00:42 +0800

    1、修改配置文件。

--- genConf.sh
+++ genConf.sh
@@ -11,6 +11,25 @@ cp ./conf/etc/* /etc/
 
 cp ./conf/var/db/turndb /var/db
 
+if [ ! -f /lib/systemd/system/avchat-turnserver.service ]; then
+
+	echo "copy avchat-turnserver.service ...."
+	cp ./conf/avchat-turnserver.service /lib/systemd/system/
+
+else
+	echo "avchat-turnserver.service is existed"
+
+fi
+
+if [ ! -f /usr/bin/avchat-turnserver-server ]; then
+
+	echo "copy avchat-turnserver-server ...."
+	cp ./conf/avchat-turnserver-server /usr/bin/
+else
+	echo "avchat-turnserver-server is existed"
+
+fi
+
 
 echo "success"
 
